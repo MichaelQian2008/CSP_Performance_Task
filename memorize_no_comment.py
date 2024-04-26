3ef3d0bbbf72f55bd6564fdfe7898b53fa50bd64
MichaelQian2008 <46621209+MichaelQian2008@users.noreply.github.com>
2024-04-26 13:09:26 +0800

    remove mode and userdata argument

--- memorize_no_comment.py
+++ memorize_no_comment.py
@@ -1,73 +1,62 @@
 import pandas as pd
 import random
-import json
 import argparse
 import warnings
 
-# Get all the arguments needed for the program to run
+
 parser = argparse.ArgumentParser(description='mem words')
 parser.add_argument('-start', dest='start', default=-1, type=int, help='begin')
 parser.add_argument('-end', dest='end', default=-1, type=int, help='end')
-
-# arguments need to be determined
 parser.add_argument('-wordDB', dest='file', default='GRE_2_revised.json', type=str, help='word file')
 args = parser.parse_args()
-
-# import word database
 data = pd.read_json(args.file)
-
-# list for test
 unknownWords = []
 allWords = []
 choices = []
-
-
 def insertChoice():
     count = 0
     choices.clear()
-    while count < 3:  # make sure that the choices don't reoccur
+    while count < 3:
         randChoice = random.choice(allWords)
-        if (randChoice not in choices) and randChoice != randWord:  # if the choices repeat
+        if (randChoice not in choices) and randChoice != randWord:
             choices.append(randChoice)
             count += 1
 
-    # insert 2 random words from the whole database
     choices.insert(random.randint(0, 4), data.content.iloc[random.randint(0, 19)]["word"]["wordHead"])
     choices.insert(random.randint(0, 5), data.content.iloc[random.randint(0, 19)]["word"]["wordHead"])
 
-    insertIndex = random.randint(0, 6)  # random the index of answer
-    choices.insert(insertIndex, randWord)  # insert the answer using the index above
+    insertIndex = random.randint(0, 6)
+    choices.insert(insertIndex, randWord)
 
     return insertIndex
 
 
-# missing start or end argument
+
 if (args.start == -1 or args.end == -1) or (args.start > args.end):
     warnings.warn("Please specify the start and end correctly using: -start and -end")
-else:  # if there is argument
-    # iterate through the words using pandas iloc by the interval user parsed
+else:
     for i in range(args.start, args.end):
         allWords.append(data.content.iloc[i]["word"]["wordHead"])
-        print(i + 1, data.content.iloc[i]["word"]["wordHead"])  # print the word
+        print(i + 1, data.content.iloc[i]["word"]["wordHead"])
         isKnownWord = input("y/n: ")
         if isKnownWord == "n":
             unknownWords.append(
-                data.content.iloc[i]["word"]["wordHead"])  # append to unknownWords list, for use in the while loop
+                data.content.iloc[i]["word"]["wordHead"])
             print("added to unknown words")
         print("===============================================================================")
-        print(data.content.iloc[i]["word"]["wordHead"], end=" ")  # print the word
+        print(data.content.iloc[i]["word"]["wordHead"], end=" ")
         try:
-            print("[" + data.content.iloc[i]["word"]["content"]["phone"] + "]")  # print the phonetics
+            print("[" + data.content.iloc[i]["word"]["content"]["phone"] + "]")
         except:
-            print("[" + data.content.iloc[i]["word"]["content"]["ukphone"] + "]")  # print the phonetics
+            print("[" + data.content.iloc[i]["word"]["content"]["ukphone"] + "]")
         print("Meaning: " + data.content.iloc[i]["word"]["content"]["trans"][0][
-            "tranOther"])  # print the meaning in english
+            "tranOther"])
         try:
             print("Sample Sentence: " + data.content.iloc[i]["word"]["content"]["sentence"]["sentences"][0][
-                "sContent"])  # print the sample sentence
+                "sContent"])
         except:
             print(
-                "Sample Sentence: Failed: This entry does not have a sample sentence.")  # print if no sample sentence
+                "Sample Sentence: Failed: This entry does not have a sample sentence.")
         print("===============================================================================")
         print()
     if len(unknownWords) != 0:
@@ -75,21 +64,21 @@ else:  # if there is argument
 
 
 while len(unknownWords) > 0:
-    randWord = random.choice(unknownWords)  # choose a word from the list
+    randWord = random.choice(unknownWords)
     for i in range(args.start, args.end):
-        if data.content.iloc[i]["word"]["wordHead"] == randWord:  # find the word in the database
+        if data.content.iloc[i]["word"]["wordHead"] == randWord:
             print("===============================================================================")
             print(data.content.iloc[i]["word"]["content"]["trans"][0]["tranOther"])
 
             insertIndex = insertChoice()
 
-            for j in range(len(choices)):  # print all the choices
+            for j in range(len(choices)):
                 print(str(j + 1), choices[j])
             print("===============================================================================")
             userChoice = int(input("Which word the meaning of the sentence [1-6]: "))
             if userChoice == insertIndex + 1:
                 print("correct")
-                unknownWords.remove(randWord)  # if correct, remove from list
+                unknownWords.remove(randWord)
             else:
                 print("wrong")
-            break  # step out of the for loop
+            break
